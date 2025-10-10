44af6a0b9426b3d1012e5e475f90c336d70c5b3e
Daniel Berg <danielbergmorais@gmail.com>
2025-10-10 16:01:02 -0300

    :sparkles: Adicionado menu de navegação entre paginas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map, mergeMap } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+    pageTitle: string = 'Were Wolf';
+    constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+        this.router.events
+            .pipe(
+                filter(event => event instanceof NavigationEnd),
+                map(() => {
+                    let route = this.activatedRoute.firstChild;
+                    while (route?.firstChild) {
+                        route = route.firstChild;
+                    }
+                    return route;
+                }),
+                filter(route => route?.outlet === 'primary'),
+                mergeMap(route => route!.data)
+            )
+            .subscribe(data => {
+                this.pageTitle = data['title'] || 'Were Wolf';
+            });
+    }
 }
