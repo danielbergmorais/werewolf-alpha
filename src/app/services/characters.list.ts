47a7a7f74ab2d708716bcc119631401e53798f0c
Daniel Berg <danielbergmorais@gmail.com>
2025-10-15 16:47:46 -0300

    :sparkles: Adicionado novos personagens do daybreak Ajuste na pagina de regras Alterado icones dos personagens

--- src/app/services/characters.list.ts
+++ src/app/services/characters.list.ts
@@ -10,7 +10,7 @@ export const charactersList = [
     {
         id: 1,
         name: 'Lobisomem',
-        img: '/assets/sprites/01-lobisomem.jpg',
+        img: '/assets/sprites/lobisomem.jpg',
         audios: [
             '/assets/audios/lobisomem/lobisomem-1.mp3',
             '/assets/audios/lobisomem/lobisomem-2.mp3',
@@ -19,8 +19,28 @@ export const charactersList = [
     },
     {
         id: 2,
+        name: 'Lobisomem Alpha',
+        img: '/assets/sprites/alpha.jpg',
+        audios: [
+            '/assets/audios/lobisomem-alpha/alpha-1.mp3',
+            '/assets/audios/lobisomem-alpha/alpha-2.mp3',
+        ],
+        interval: 2500,
+    },
+    {
+        id: 3,
+        name: 'Lobisomem Místico',
+        img: '/assets/sprites/mystic.jpg',
+        audios: [
+            '/assets/audios/lobisomem-mistico/mystic-1.mp3',
+            '/assets/audios/lobisomem-mistico/mystic-2.mp3',
+        ],
+        interval: 2500,
+    },
+    {
+        id: 4,
         name: 'Minion',
-        img: '/assets/sprites/02-minion.jpg',
+        img: '/assets/sprites/minion.jpg',
         audios: [
             '/assets/audios/minion/minion-1.mp3',
             '/assets/audios/minion/minion-2.mp3',
@@ -28,9 +48,9 @@ export const charactersList = [
         interval: 2500,
     },
     {
-        id: 3,
+        id: 5,
         name: 'Mason',
-        img: '/assets/sprites/03-mason.jpg',
+        img: '/assets/sprites/mason.jpg',
         audios: [
             '/assets/audios/mason/mason-1.mp3',
             '/assets/audios/mason/mason-2.mp3',
@@ -38,9 +58,9 @@ export const charactersList = [
         interval: 2000,
     },
     {
-        id: 4,
+        id: 6,
         name: 'Vidente',
-        img: '/assets/sprites/04-vidente.jpg',
+        img: '/assets/sprites/vidente.jpg',
         audios: [
             '/assets/audios/vidente/vidente-1.mp3',
             '/assets/audios/vidente/vidente-2.mp3',
@@ -48,9 +68,19 @@ export const charactersList = [
         interval: 3000,
     },
     {
-        id: 5,
+        id: 7,
+        name: 'Aprendiz de Vidente',
+        img: '/assets/sprites/aprendiz-vidente.jpg',
+        audios: [
+            '/assets/audios/vidente-aprendiz/aprendiz-1.mp3',
+            '/assets/audios/vidente-aprendiz/aprendiz-2.mp3',
+        ],
+        interval: 3000,
+    },
+    {
+        id: 8,
         name: 'Investigador Paranormal',
-        img: '/assets/sprites/05-c-investigador-paranormal.jpg',
+        img: '/assets/sprites/investigador.jpg',
         audios: [
             '/assets/audios/investigador-paranormal/investigador-paranormal-1.mp3',
             '/assets/audios/investigador-paranormal/investigador-paranormal-2.mp3',
@@ -58,9 +88,9 @@ export const charactersList = [
         interval: 2000,
     },
     {
-        id: 6,
+        id: 9,
         name: 'Ladrão',
-        img: '/assets/sprites/05-ladrao.jpg',
+        img: '/assets/sprites/ladrao.jpg',
         audios: [
             '/assets/audios/ladrao/ladrao-1.mp3',
             '/assets/audios/ladrao/ladrao-2.mp3',
@@ -68,9 +98,9 @@ export const charactersList = [
         interval: 2000,
     },
     {
-        id: 7,
+        id: 10,
         name: 'Bruxa',
-        img: '/assets/sprites/06-b-bruxa.jpg',
+        img: '/assets/sprites/bruxa.jpg',
         audios: [
             '/assets/audios/bruxa/bruxa-1.mp3',
             '/assets/audios/bruxa/bruxa-2.mp3',
@@ -78,9 +108,9 @@ export const charactersList = [
         interval: 2000,
     },
     {
-        id: 8,
+        id: 11,
         name: 'Encrenqueira',
-        img: '/assets/sprites/06-encrenqueira.jpg',
+        img: '/assets/sprites/encrenqueira.jpg',
         audios: [
             '/assets/audios/encrenqueira/encrenqueira-1.mp3',
             '/assets/audios/encrenqueira/encrenqueira-2.mp3',
@@ -88,9 +118,19 @@ export const charactersList = [
         interval: 4000,
     },
     {
-        id: 9,
+        id: 12,
+        name: 'Idiota da Vila',
+        img: '/assets/sprites/idiota-vila.jpg',
+        audios: [
+            '/assets/audios/idiota-da-vila/idiota-1.mp3',
+            '/assets/audios/idiota-da-vila/idiota-2.mp3',
+        ],
+        interval: 6000,
+    },
+    {
+        id: 13,
         name: 'Bêbado',
-        img: '/assets/sprites/07-bebado.jpg',
+        img: '/assets/sprites/bebado.jpg',
         audios: [
             '/assets/audios/bebado/bebado-1.mp3',
             '/assets/audios/bebado/bebado-2.mp3',
@@ -98,14 +138,34 @@ export const charactersList = [
         interval: 3000,
     },
     {
-        id: 10,
+        id: 14,
         name: 'Sonâmbula',
-        img: '/assets/sprites/08-sonambula.jpg',
+        img: '/assets/sprites/sonambula.jpg',
         audios: [
             '/assets/audios/sonambula/sonambula-1.mp3',
             '/assets/audios/sonambula/sonambula-2.mp3',
         ],
         interval: 3000,
     },
+    {
+        id: 15,
+        name: 'Revelador',
+        img: '/assets/sprites/revelador.jpg',
+        audios: [
+            '/assets/audios/revelador/revelador-1.mp3',
+            '/assets/audios/revelador/revelador-2.mp3',
+        ],
+        interval: 3000,
+    },
+    /*{
+        id: 16,
+        name: 'Curador',
+        img: '/assets/sprites/curador.jpg',
+        audios: [
+            '/assets/audios/curador/curador-1.mp3',
+            '/assets/audios/curador/curador-2.mp3',
+        ],
+        interval: 3000,
+    },*/
 ]
 
